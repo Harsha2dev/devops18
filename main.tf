d1d2a83c2b10cb8e15d5d907dfbc88ac43ceac51
sahajaranjani <138554144+sahajaranjani@users.noreply.github.com>
2026-03-13 11:46:13 +0530

    Rename resources in main.tf for consistency

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "newproject"
+    name = "newproject1"
     image_id           = "ami-0b0b78dcacbab728f"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
@@ -12,8 +12,8 @@ resource "aws_launch_template" "web_server_as" {
    
 
 
-  resource "aws_elb" "web_server_2b"{
-     name = "web-server-2b"
+  resource "aws_elb" "web_server_22b"{
+     name = "web-server-22b"
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-0d18ad490c15f35e5", "subnet-00a7d29eb203c35d0"]
      listener {
@@ -26,13 +26,13 @@ resource "aws_launch_template" "web_server_as" {
       Name = "terraform-elb"
     }
   }
-resource "aws_autoscaling_group" "web_server_asg_devops" {
-    name                 = "web-server-asg_devops"
+resource "aws_autoscaling_group" "web_server_asg_devops1" {
+    name                 = "web-server-asg_devops1"
     min_size             = 1
     max_size             = 3
     desired_capacity     = 2
     health_check_type    = "EC2"
-    load_balancers       = [aws_elb.web_server_2b.name]
+    load_balancers       = [aws_elb.web_server_22b.name]
     availability_zones    = ["us-east-2a", "us-east-2b"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id

d576a918e21437c94e5e68160ba28c8342d9fe6d
sahajaranjani <138554144+sahajaranjani@users.noreply.github.com>
2026-03-13 10:29:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "myproject"
+    name = "newproject"
     image_id           = "ami-0b0b78dcacbab728f"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
